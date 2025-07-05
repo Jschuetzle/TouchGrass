ae4509d30fe95586e19607b55de7387510d04f99
Jackson Schuetzle <jschuetzle22@gmail.com>
2025-07-05 14:19:56 -0500

    Enable all origins for CORS

--- touchgrass-backend/src/main.ts
+++ touchgrass-backend/src/main.ts
@@ -6,6 +6,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    credentials: false,
+  })
+
   // Enable validation globally
   app.useGlobalPipes(new ValidationPipe());
 
