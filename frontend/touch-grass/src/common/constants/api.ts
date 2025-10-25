24b10e73e6a64863e8f2c6fcb82de52568c24df9
RyanL72 <94211083+Ryanlin2@users.noreply.github.com>
2025-10-25 12:39:16 -0400

    models implemented

--- frontend/touch-grass/src/common/constants/api.ts
+++ frontend/touch-grass/src/common/constants/api.ts
@@ -1,8 +1,8 @@
-import Constants from 'expo-constants';
+import Constants from "expo-constants";
 
 export enum DashboardStatus {
-    NEW_USER = 'NEW_USER',
-    EXISTING_USER = 'EXISTING_USER',
+  NEW_USER = "NEW_USER",
+  EXISTING_USER = "EXISTING_USER",
 }
 
-export const BASE_URL = `http://${Constants.expoConfig.extra.backendIP}`;
+export const BASE_URL = `http://${Constants.expoConfig.extra.backendIP}:${Constants.expoConfig.extra.backendPort}`;
