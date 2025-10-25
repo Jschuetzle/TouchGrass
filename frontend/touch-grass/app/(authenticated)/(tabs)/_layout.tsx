fb86788c11fbc3001f899ff375d0575cca851b9b
Jackson Schuetzle <jschuetzle22@gmail.com>
2025-10-25 00:27:39 -0400

    Added 'initialRouteName' to avoid friends tab from being loaded first.

--- frontend/touch-grass/app/(authenticated)/(tabs)/_layout.tsx
+++ frontend/touch-grass/app/(authenticated)/(tabs)/_layout.tsx
@@ -3,7 +3,8 @@ import { Ionicons, FontAwesome } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
-    <Tabs 
+    <Tabs
+      initialRouteName="index"
       screenOptions={{
         headerShown: false,
       }}
