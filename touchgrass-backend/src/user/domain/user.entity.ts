58c5d2cfc080a0fbbfe0e381499aac66df29a3c4
Jackson Schuetzle <jschuetzle22@gmail.com>
2025-12-21 20:25:34 -0600

    moved user entity to domain folder

--- touchgrass-backend/src/user/domain/user.entity.ts
+++ touchgrass-backend/src/user/domain/user.entity.ts
@@ -14,6 +14,7 @@ import { IsBoolean, IsDate, IsEmail, IsInt, IsNotEmpty, IsOptional, IsString, Is
 import { IsNotUndefined } from '../../common/decorators/class-validator';
 
 @Entity('user')
+@Unique(USERNAME_CONSTRAINT_NAME, ['username'])
 export class User {
   @PrimaryColumn({ 
     type: 'varchar', 
