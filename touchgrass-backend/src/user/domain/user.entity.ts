3bdda05a282bc237cece866004ba497130cad9b4
Jackson Schuetzle <jschuetzle22@gmail.com>
2025-12-23 22:32:23 -0600

    added custom decorator for validation on undefined values inside entities

--- touchgrass-backend/src/user/domain/user.entity.ts
+++ touchgrass-backend/src/user/domain/user.entity.ts
@@ -14,6 +14,7 @@ import { IsBoolean, IsDate, IsEmail, IsInt, IsNotEmpty, IsOptional, IsString, Is
 import { IsNotUndefined } from '../../common/decorators/class-validator';
 
 @Entity('user')
+@Expose()
 export class User {
   @PrimaryColumn({ 
     type: 'varchar', 
