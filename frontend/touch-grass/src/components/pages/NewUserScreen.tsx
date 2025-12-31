fb39c53b6206e13c259101a7fc9bcc3758d58679
Ryan_Lin2 <Ryanlin0861@gmail.com>
2025-12-31 10:55:46 -0500

    clean up

--- frontend/touch-grass/src/components/pages/NewUserScreen.tsx
+++ frontend/touch-grass/src/components/pages/NewUserScreen.tsx
@@ -34,7 +34,6 @@ export default function NewUserScreen() {
 
   const router = useRouter();
 
-  // use information from provider data to prefill input fields (i.e. set state)
   useEffect(() => {
     const splitDisplayName = firebaseProviderData.displayName?.split(' ') ?? [""];
     console.log(splitDisplayName);
