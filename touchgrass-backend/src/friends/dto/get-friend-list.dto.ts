ae9cd19c8a0a9738cc8b988b436df0891c89bc58
RyanL72 <94211083+Ryanlin2@users.noreply.github.com>
2026-01-05 22:29:08 -0500

    Proper Usage of FriendDto and GetFriendListDto

--- touchgrass-backend/src/friends/dto/get-friend-list.dto.ts
+++ touchgrass-backend/src/friends/dto/get-friend-list.dto.ts
@@ -1,16 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { FriendDto } from './friend.dto';
+import { Expose , Type } from 'class-transformer'
 
 export class GetFriendListDto {
   @ApiProperty({ example: 23 })
+  @Expose()
   total: number;
 
   @ApiProperty({ example: 1 })
+  @Expose()
   page: number;
 
   @ApiProperty({ example: 10 })
+  @Expose()
   limit: number;
 
   @ApiProperty({ type: [FriendDto] })
+  @Expose()
+  @Type(() => FriendDto)
   results: FriendDto[];
 }
