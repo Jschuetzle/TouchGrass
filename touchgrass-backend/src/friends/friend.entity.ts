55b59b40513f064986f610f85d31103d8bcc28a9
Jackson Schuetzle <jschuetzle22@gmail.com>
2026-01-06 22:23:21 -0600

    fixed indenting

--- touchgrass-backend/src/friends/friend.entity.ts
+++ touchgrass-backend/src/friends/friend.entity.ts
@@ -8,42 +8,42 @@ import {
   import { User } from '../user/domain/user.entity';
 import { FOLLOWEDID_FOLLOWS_CONSTRAINT_NAME, FOLLOWINGID_FOLLOWS_CONSTRAINT_NAME, FOLLOWS_PK_CONSTRAINT_NAME } from '../common/constants/db-constraints';
   
-  @Entity('follows')
-  export class Follow {
-    @PrimaryColumn({ 
-      type: 'varchar', 
-      length: 128,
-      primaryKeyConstraintName: FOLLOWS_PK_CONSTRAINT_NAME, 
-    })
-    following_id: string;
-    
-    @PrimaryColumn({ 
-      type: 'varchar', 
-      length: 128,
-      primaryKeyConstraintName: FOLLOWS_PK_CONSTRAINT_NAME, 
-    })
-    followed_id: string;
-    
-    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-    requested_at: Date;
-
-    @Column({ type: 'boolean', default: true })
-    is_pending: boolean;
+@Entity('follows')
+export class Follow {
+  @PrimaryColumn({ 
+    type: 'varchar', 
+    length: 128,
+    primaryKeyConstraintName: FOLLOWS_PK_CONSTRAINT_NAME, 
+  })
+  following_id: string;
   
-    @Column({ type: 'timestamp', nullable: true })
-    accepted_at: Date;
+  @PrimaryColumn({ 
+    type: 'varchar', 
+    length: 128,
+    primaryKeyConstraintName: FOLLOWS_PK_CONSTRAINT_NAME, 
+  })
+  followed_id: string;
+  
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  requested_at: Date;
 
-    @ManyToOne(() => User, user => user.following, { onDelete: 'CASCADE' })
-    @JoinColumn({ 
-      name: 'following_id',
-      foreignKeyConstraintName: FOLLOWINGID_FOLLOWS_CONSTRAINT_NAME,
-    })
-    following: User;
-    
-    @ManyToOne(() => User, user => user.followers, { onDelete: 'CASCADE' })
-    @JoinColumn({ 
-      name: 'followed_id',
-      foreignKeyConstraintName: FOLLOWEDID_FOLLOWS_CONSTRAINT_NAME,
-    })
-    followed: User;
-  }
+  @Column({ type: 'boolean', default: true })
+  is_pending: boolean;
+
+  @Column({ type: 'timestamp', nullable: true })
+  accepted_at: Date;
+
+  @ManyToOne(() => User, user => user.following, { onDelete: 'CASCADE' })
+  @JoinColumn({ 
+    name: 'following_id',
+    foreignKeyConstraintName: FOLLOWINGID_FOLLOWS_CONSTRAINT_NAME,
+  })
+  following: User;
+  
+  @ManyToOne(() => User, user => user.followers, { onDelete: 'CASCADE' })
+  @JoinColumn({ 
+    name: 'followed_id',
+    foreignKeyConstraintName: FOLLOWEDID_FOLLOWS_CONSTRAINT_NAME,
+  })
+  followed: User;
+}
